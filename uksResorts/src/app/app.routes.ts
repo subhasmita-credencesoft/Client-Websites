91361721f2a55eced1601de01c5e38c7438c51ee
subhasmita <subhasmita.tripathy@credencesoft.co.nz>
2025-12-22 19:15:32 +0530

    fixed the contact us page

--- uksResorts/src/app/app.routes.ts
+++ uksResorts/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,8 @@ export const routes: Routes = [
     {
     path: 'gallery',
     component: GalleryComponent
+  },    {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
