b70cd6564c4dcc61ad6a31b6e6ae004917c3457d
subhasmita <subhasmita.tripathy@credencesoft.co.nz>
2026-01-26 13:21:43 +0530

    new event pages and routing

--- uksResorts/src/app/app.routes.ts
+++ uksResorts/src/app/app.routes.ts
@@ -9,6 +9,10 @@ import { FacilitiesComponent } from './facilities/facilities.component';
 import { TermsConditionComponent } from './terms-condition/terms-condition.component';
 import { PolicyComponent } from './policy/policy.component';
 import { BlogComponent } from './blog/blog.component';
+import { OneDayPicnicComponent } from './one-day-picnic/one-day-picnic.component';
+import { WeddingComponent } from './wedding/wedding.component';
+import { SportsComponent } from './sports/sports.component';
+import { ConferenceComponent } from './conference/conference.component';
 
 export const routes: Routes = [
   {
@@ -41,6 +45,22 @@ export const routes: Routes = [
   {
     path: 'policy',
     component: PolicyComponent
+  },
+  {
+    path: 'picnic',
+    component: OneDayPicnicComponent
+  },
+  {
+    path: 'wedding',
+    component: WeddingComponent
+  },
+  {
+    path: 'sports',
+    component: SportsComponent
+  },
+   {
+    path: 'conference',
+    component: ConferenceComponent
   },
   {
     path: 'terms-condition',
---
+++ uksResorts/src/app/one-day-picnic/one-day-picnic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-day-picnic',
+  standalone: true,
+  imports: [],
+  templateUrl: './one-day-picnic.component.html',
+  styleUrl: './one-day-picnic.component.scss'
+})
+export class OneDayPicnicComponent {
+
+}
