30387d3c7366f552afdea0993046c28d41aaeaab
subhasmita <subhasmita.tripathy@credencesoft.co.nz>
2026-01-26 19:03:47 +0530

    fixed

--- uksResorts/src/app/reservation/reservation.component.ts
+++ uksResorts/src/app/reservation/reservation.component.ts
@@ -1,12 +1,26 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { NgbDateStruct, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [],
+  imports: [NgbModule,FormsModule],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss'
 })
 export class ReservationComponent {
+  todayDate: NgbDateStruct;
+  fromDate!: NgbDateStruct;
+  toDate!: NgbDateStruct;
+
+  constructor() {
+    const today = new Date();
+    this.todayDate = {
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      day: today.getDate()
+    };
+  }
 
 }
