0d5816e49aaf08600a01a41e3953725adcd9dfe8
swadhinjit-das <134703660+shakti-credencesoft@users.noreply.github.com>
2026-03-09 20:01:27 +0530

    about section chnaged

--- shivSewa/src/app/customer/pages/home/about-us/about-us.component.ts
+++ shivSewa/src/app/customer/pages/home/about-us/about-us.component.ts
@@ -1,17 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonModule, CardModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  expanded = false;
 
+  toggleContent(): void {
+    this.expanded = !this.expanded;
+  }
 
-
-  scrollingtoTop(){
+  scrollingtoTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
