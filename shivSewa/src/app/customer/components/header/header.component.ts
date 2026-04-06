827254e7f21b083e410442d4d4b93fd2eb47a90a
Binaya Rout <binaya@credencesoft.in>
2026-04-06 14:53:41 +0530

    whatsapp booking button

--- shivSewa/src/app/customer/components/header/header.component.ts
+++ shivSewa/src/app/customer/components/header/header.component.ts
@@ -31,4 +31,18 @@ scrollTo(sectionId: string) {
 
   this.menuOpen = false; // close mobile menu after clicking
 }
+
+openWhatsApp() {
+  const message = 
+    'Hello Shiv Sewa! I am looking to book a ride from your website.' +
+    '%0A%0AMy Trip Details:' +
+    '%0A• Pickup:' +
+    '%0A• Drop:' +
+    '%0A• Date:' +
+    '%0A• Car:' +
+    '%0A%0APlease send me a quote. Thanks!';
+
+  const url = `https://wa.me/919136399064?text=${message}`;
+  window.open(url, '_blank');
+}
 }
