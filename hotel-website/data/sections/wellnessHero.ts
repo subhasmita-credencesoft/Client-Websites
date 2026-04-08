6b2de0760e4fe2283ec24deffa13d8bc22ff4c45
swadhinjit-das <swadhinjit@credencesoft.in>
2026-04-08 18:38:00 +0530

    picnic  image changed

--- hotel-website/data/sections/wellnessHero.ts
+++ hotel-website/data/sections/wellnessHero.ts
@@ -18,7 +18,7 @@ export const WELLNESS_HERO_SLIDES: WellnessHeroSlide[] = [
     id: "picnic",
     label: "Picnic",
     headline: "Enjoy peaceful outdoor picnics in scenic surroundings",
-    image: "/corporate2.avif",
+    image: "/children7.avif",
     href: "https://www.google.co.in/maps/@18.8171575,73.3046448,3a,90y,119.21h,85.89t/data=!3m8!1e1!3m6!1s2c65xsf3YxUAAAQvxYn66g!2e0!3e2!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D4.109999999999999%26panoid%3D2c65xsf3YxUAAAQvxYn66g%26yaw%3D119.21!7i13312!8i6656?entry=ttu&g_ep=EgoyMDI2MDMxNS4wIKXMDSoASAFQAw%3D%3D",
   },
   {
