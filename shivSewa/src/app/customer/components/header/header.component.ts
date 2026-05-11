64fcb28537b5daab6c57aa4c1c732b875ece5b35
Binaya Rout <binaya@credencesoft.in>
2026-05-11 16:49:24 +0530

    fix

--- shivSewa/src/app/customer/components/header/header.component.ts
+++ shivSewa/src/app/customer/components/header/header.component.ts
@@ -38,7 +38,8 @@ openWhatsApp() {
     '%0A%0AMy Trip Details:' +
     '%0A• Pickup:' +
     '%0A• Drop:' +
-    '%0A• Date & Time:' +
+    '%0A• Date:' +
+    '%0A• Time:' +
     '%0A• Car:' +
     '%0A%0APlease send me a quote. Thanks!';
 
