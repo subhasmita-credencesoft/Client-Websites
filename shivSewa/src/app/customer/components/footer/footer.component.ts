ffe19ea8d1144d1cc61899c704476d64d3aee066
Binaya Rout <binaya@credencesoft.in>
2026-06-04 16:51:31 +0530

    location fix

--- shivSewa/src/app/customer/components/footer/footer.component.ts
+++ shivSewa/src/app/customer/components/footer/footer.component.ts
@@ -32,7 +32,7 @@ companyLinks: NavLink[] = [
   ];
 
   contactInfo: ContactInfo[] = [
-    { icon: 'location', label: 'C-6-10-4-1, Sector6, CBD Belapur, Thane- 400614, Maharashtra', value: '' },
+    { icon: 'location', label: 'C-6-10-4-1, Sector6, CBD Belapur, Navi Mumbai - 400614, Maharashtra', value: '' },
     { icon: 'phone', label: '9136399064', value: '' },
     { icon: 'email', label: 'rides@shivsewa.in', value: '' }
   ];
